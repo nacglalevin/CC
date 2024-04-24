cb94060de1bb01ca66f745d8b63df07ce3d486c7
Lalevin <167055481+nacglalevin@users.noreply.github.com>
2024-04-24 22:14:14 +0800

    更新 cc.1.0.py

--- cc.1.0.py
+++ cc.1.0.py
@@ -2,7 +2,7 @@
 ========================================
 Name:DDos.3.0  Author: Lalevin Martin
  Mailbox: zzlyxht@outlook.com                                                
- Github: http://github.com/zzlyxht@outlook.com
+ Github: http://github.com/nacglalevin
 Written in 2024-4-24
 ==================NACG==================
 """
